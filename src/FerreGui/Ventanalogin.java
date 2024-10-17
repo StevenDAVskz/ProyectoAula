a175ac2be75bd16be32bda07ee66de75c3eb8498
StevenDAVskz <steven.deaguasvivero@unicolombo.edu.co>
2024-10-17 07:12:15 -0500

    Cachon

--- src/FerreGui/Ventanalogin.java
+++ src/FerreGui/Ventanalogin.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package ferrefactura.GUI;
+package FerreGui;
 
 import javax.swing.JTextField;
 
