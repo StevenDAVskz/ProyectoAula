17c7a90f69e1fb35418a197e92c5df7ef9874549
FaberVillarreal18 <faber.villarrealantequera@unicolombo.edu.co>
2024-10-19 02:47:28 -0500

    DeleteFacturacion

---
+++ src/ferrefactura/Clases/Comand/DeleteFacturacion.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ferrefactura.Clases.Comand;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class DeleteFacturacion {
+    private int idfactura;
+    
+    public DeleteFacturacion(int id){
+        this.idfactura = idfactura;
+    }
+    
+    public int getIdfactura(){
+        return idfactura;
+    }
+}
