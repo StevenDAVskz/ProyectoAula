a57f065d464ea5b4ed1e68dd847e956f7c36c56f
StevenDAVskz <steven.deaguasvivero@unicolombo.edu.co>
2024-10-25 15:25:31 -0500

    Gui

--- src/ferrefactura/Clases/Comand/UpdateFacturacion.java
+++ src/ferrefactura/Clases/Comand/UpdateFacturacion.java
@@ -4,10 +4,40 @@
  */
 package ferrefactura.Clases.Comand;
 
+import ferrefactura.Clases.Productos;
+import ferrefactura.Clases.Trabajadores;
+import ferrefactura.Clases.Usuarios;
+
 /**
  *
  * @author LENOVO
  */
 public class UpdateFacturacion {
-    
+      private int idfactura; 
+    private Usuarios user;
+    private Productos productos;
+    private Trabajadores trabajador;
+
+    public UpdateFacturacion(int idfactura, Usuarios user, Productos productos, Trabajadores trabajador) {
+        this.idfactura = idfactura;
+        this.user = user;
+        this.productos = productos;
+        this.trabajador = trabajador;
+    }
+
+    public int getIdfactura() {
+        return idfactura;
+    }
+
+    public Usuarios getUser() {
+        return user;
+    }
+
+    public Productos getProductos() {
+        return productos;
+    }
+
+    public Trabajadores getTrabajador() {
+        return trabajador;
+    }
 }
